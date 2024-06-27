9404851ab2bbc8dc60a3daabe025e19e633af26c
“xiexie0” <nym980617@163.com>
2024-06-27 09:23:42 +0800

    0627

--- python-course/test.py
+++ python-course/test.py
@@ -1,12 +1,8 @@
-# 从用户获取两个数字并计算它们的和
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
+# 输入两个数字并计算他们的和并输出
+num1 = input("Please input the first number: " ) 
+num2 = input("Please input the second number: ")
 
-# 将输入的字符串转换为整数
 num1 = int(num1)
 num2 = int(num2)
 
-# 计算和并输出结果
-total = num1 + num2
-print("The sum of", num1, "and", num2, "is", total)
-1
+print(num1 + num2)
